a05025df07b0a3a4b956df681d26e4df6a1d99ee
iwasatakahito <iwasa.takahito@classmethod.jp>
2022-03-25 18:53:31 +0900

    minor ver up.

--- tf-vpc/v1/infrastructure/environment.tf
+++ tf-vpc/v1/infrastructure/environment.tf
@@ -1,21 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-  backend "s3" {
-    bucket = "iwasa-terraform-state-bucket"
-    key    = "tf-vpc/terraform.tfstate"
-    region = "ap-northeast-1"
-  }
-}
-
-provider "aws" {
-  region = "ap-northeast-1"
-}
-
 # VPC
 resource "aws_vpc" "tf-hoge-vpc" {
   cidr_block           = var.environment.inputs.vpc_cidr
